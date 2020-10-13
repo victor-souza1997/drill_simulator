5b7e0956b568fd70eff28c25d4b3ce1be10dd836
Joao Victor Lima de SOuza <victor-souza1997@localhost.localdomain>
2020-10-13 00:35:22 -0300

    classe para o cálculo do fluido pela leis das massas concluida.

--- source/drill_simulation.cpp
+++ source/drill_simulation.cpp
@@ -23,15 +23,25 @@ class Fluid{ //classe representado os fluidos
 		double fluid_visconsity;//viscosidade do fluido
 		double rho_f; //massa específica do fluido
 		double q; //volume por unidade de tempo
-		double v;
-		double Re;
-		double f;
-		double Perda_f;
+		double v; //Velocidade média do fluido
+		double Re; // Número de Reynolds
+		double f; //Constante de fricção
+		double Perda_f; //A perda será comum ao fluido
 		Fluid(double a, double b){
 			theta_300 = a;
 			theta_600 = b;
 			 //viscosidade comum para todos os fluidos
 		};
+		void set_velocity(double D){//Cálculo da velocidade média do fluido, comum a todos os fluidos
+			v =  q/(2.448*pow(D,2));
+		};
+		void set_Reynolds(double D){//Calculo do número de Reynolds, comum a todos os fluidos
+			Re = 928*rho_f*v*D/fluid_visconsity;
+		};
+		void set_friction_Laminar(){// Calculo da fricção laminar, comum a todos os fluidos
+			f = 16/Re;
+		};
+		
 };
 class Fluid_Newton: public Fluid{ // classe representando aos fluidos newtonianos
 	public:
@@ -40,21 +50,13 @@ class Fluid_Newton: public Fluid{ // classe representando aos fluidos newtoniano
 		void set_visconsity(){
 			fluid_visconsity = theta_300; //viscosidade do fluido newtoniano
 		};
-		void set_velocity(double D){//Cálculo da velocidade média do fluido
-			v =  q/(2.448*pow(D,2));
-		};
-		void set_Reynolds(double D){//Calculo do número de Reynolds
-			Re = 928*rho_f*v*D/fluid_visconsity;
-		};
-		void set_friction_Laminar(){
-			f = 16/Re;
-		};
+		
 		void set_friction_turbulent(){ //calculo da friccao turbulenta é feito interativamente
 			double f_i_1 = 0.01;
 			float error = 10;
 			double F, F_, f_i;	
 			while(error>0.01){//o codigo irá rodar até que o erro seja menor que 1%
-				F = 4*log(Re*sqrt(f_i_1))-1/sqrt(f_i_1)-0.395;
+				F = 4*log10(Re*sqrt(f_i_1))-1/sqrt(f_i_1)-0.395;
 				F_ = -2/(Re*pow(f_i_1,2))+1/(2*sqrt(pow(f_i_1,3)));
 				f_i = f_i_1-F/F_;
 				error = sqrt(pow((f_i_1-f_i),2));
@@ -80,20 +82,14 @@ class Fluid_Elastic: public Fluid{ //classe representado aos fluidos elásticos
 		double fluid_Shear_stress0; //variável representado a constante de cisalhamento necessária para que o fluido seja deformado
 		double He, Re_c;
 		
-		void set_visconsity(){
+		void set_visconsity(){// Encontrar a viscosidade do fluido, junto com a tensão de cisalhamento inicial para que o fluido se deforme
 			fluid_visconsity = theta_600-theta_300; 
 			fluid_Shear_stress0 = theta_300  - fluid_visconsity;
 		};
-		void set_velocity(double D){//Cálculo da velocidade média do fluido
-			v =  q/(2.448*pow(D,2));
-		};
-		void set_Reynolds(double D){//Calculo do número de Reynolds
-			Re = 928*rho_f*v*D/fluid_visconsity;
-		};
 		void set_Hedstron(double D){
-			He = 37100*rho_f*fluid_Shear_stress0*D/pow(fluid_visconsity,2);
+			He = 37100*rho_f*fluid_Shear_stress0*D/pow(fluid_visconsity,2); // Encontrar o número de Hedstron
 		};
-		void set_Critical_Reynolds(){
+		void set_Critical_Reynolds(){ //Funcao para calcular o valor de Reynolds crítico
 			double x_i_1 = 1;
 			double x_i;
 			float error = 10;
@@ -108,15 +104,12 @@ class Fluid_Elastic: public Fluid{ //classe representado aos fluidos elásticos
 			};
 			Re_c = x_i;
 		};
-		void set_friction_Laminar(){
-			f = 16/Re;
-		};
 		void set_friction_turbulent(){ //calculo da friccao turbulenta é feito interativamente
 			double f_i_1 = 0.01;
 			float error = 10;
 			double F, F_, f_i;	
 			while(error>0.01){//o codigo irá rodar até que o erro seja menor que 1%
-				F = 4*log(Re*sqrt(f_i_1))-1/sqrt(f_i_1)-0.395;
+				F = 4*log10(Re*sqrt(f_i_1))-1/sqrt(f_i_1)-0.395;
 				F_ = -2/(Re*pow(f_i_1,2))+1/(2*sqrt(pow(f_i_1,3)));
 				f_i = f_i_1-F/F_;
 				error = sqrt(pow((f_i_1-f_i),2));
@@ -142,10 +135,37 @@ class Fluid_Power_Law: public Fluid{ //classe repressentando aos fluidos modelad
 	public:
 		double fluid_n_power_law;
 		double fluid_K_power_law;
-		void set_visconsity(int n){
-			fluid_n_power_law = 3.32*log(theta_600/theta_300); 
-			fluid_K_power_law = 510*theta_300/(pow(510,n));
+		double Re_c;
+		void set_visconsity(double D){
+			fluid_n_power_law = 3.32*log10(theta_600/theta_300); 
+			fluid_K_power_law = 510*theta_300/(pow(510,fluid_n_power_law));
+			fluid_visconsity = fluid_K_power_law/96*pow(D/v,1-fluid_n_power_law)*pow((3+1/fluid_n_power_law)/0.0416,fluid_n_power_law);
 		};	
+		void set_friction_turbulent(){ //calculo da friccao turbulenta é feito interativamente
+			double f_i_1 = 0.001;
+			float error = 10;
+			double F, F_, f_i;	
+			while(error>0.01){//o codigo irá rodar até que o erro seja menor que 1%
+				F = 4*log10(Re*pow(f_i_1,1-fluid_n_power_law/2))/pow(fluid_n_power_law,0.75)-1/sqrt(f_i_1)-0.395/pow(fluid_n_power_law,1.2);
+				F_ = 1/(2*pow(f_i_1,1.5))+4*(1-0.5*fluid_n_power_law)/(f_i_1*pow(fluid_n_power_law,0.75)*log(10));
+				f_i = f_i_1-F/F_;
+				error = sqrt(pow((f_i_1-f_i),2));
+				f_i_1 = f_i;
+			};
+			f = f_i; 
+		};
+		double get_fluid_loss(double D){//Cálculo da perda de carga para um determinado solo
+			set_velocity(D);
+			set_Reynolds(D);
+			if(Re>Re_c) set_friction_Laminar();
+		
+			else set_friction_turbulent();
+		
+			Perda_f =  f*rho_f*pow(v,2)/(25,8*D);
+			return Perda_f;
+		};
+
+
 	};
 
 class Drill{ //classe para a broca
